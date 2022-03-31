c23266658d97be986d88419ab5c9825ec76ecc42
masli <masli@10.50.33.51>
2022-03-31 06:34:13 +0100

    the first commit

---
+++ Project/src/Manager.java
@@ -0,0 +1,14 @@
+
+public class Manager extends Driver {
+
+	public Manager() {
+		super();
+	}
+
+	public Manager(String username, String password) {
+		super(username, password);
+	
+	}
+
+	
+}
---
+++ Project/src/Vehical.java
@@ -0,0 +1,96 @@
+import java.util.Date;
+
+public class Vehical {
+
+	String Make;
+	String Model;
+	int Weight;
+	String regNo;
+	boolean availablity;
+	WorkSchedule schedule;
+	
+	
+	
+	
+		public Vehical() {
+		
+		}
+
+
+		public Vehical(String make, String model, int weight, String regNo) {
+		super();
+		Make = make;
+		Model = model;
+		Weight = weight;
+		this.regNo = regNo;
+		
+	}
+
+
+		public boolean isAvailable()
+		{
+		return this.availablity;
+		}
+	
+		
+		public void setSchedule(String client, Date startDate, Date endDate) {
+			
+			this.schedule=new WorkSchedule(client,startDate,endDate);
+		}
+
+
+		public String getMake() {
+			return Make;
+		}
+
+
+		public void setMake(String make) {
+			Make = make;
+		}
+
+
+		public String getModel() {
+			return Model;
+		}
+
+
+		public void setModel(String model) {
+			Model = model;
+		}
+
+
+		public int getWeight() {
+			return Weight;
+		}
+
+
+		public void setWeight(int weight) {
+			Weight = weight;
+		}
+
+
+		public String getRegNo() {
+			return regNo;
+		}
+
+
+		public void setRegNo(String regNo) {
+			this.regNo = regNo;
+		}
+
+
+		public boolean isAvailablity() {
+			return availablity;
+		}
+
+
+		public void setAvailablity(boolean availablity) {
+			this.availablity = availablity;
+		}
+
+
+		
+		
+		
+}
+
